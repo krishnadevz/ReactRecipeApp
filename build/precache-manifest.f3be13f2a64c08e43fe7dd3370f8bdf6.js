6d062a83ee7904c36c635c2912bde5f03396866f
krishna kakade <krishnakakade77@gmail.com>
2020-06-24 21:12:38 +0530

    npm run build added

---
+++ build/precache-manifest.f3be13f2a64c08e43fe7dd3370f8bdf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6031249631ef73c13594b46c37ce3f40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9deec173ea2f19dd1125",
+    "url": "/static/css/main.be1afd25.chunk.css"
+  },
+  {
+    "revision": "a82b6b9f94db86c76682",
+    "url": "/static/js/2.d03a7f69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d03a7f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9deec173ea2f19dd1125",
+    "url": "/static/js/main.688a10d2.chunk.js"
+  },
+  {
+    "revision": "7c5afba548fa511cfc57",
+    "url": "/static/js/runtime-main.5bf057b0.js"
+  }
+]);
